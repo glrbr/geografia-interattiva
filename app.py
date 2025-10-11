917af9ddd1c1d0aac1ac55e3c4dd18e451ac9bc3
glrbr <gloria12brivio@gmail.com>
2025-10-11 15:02:48 +0200

    prima app con mappa

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as sl
+sl.title("Geografia interattiva")
+sl.write("ecco il mio progetto di esame")
+sl.image("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Map_of_the_world_with_countries_and_dependencies.svg/1024px-Map_of_the_world_with_countries_and_dependencies.svg.png")
+sl.success("la mia prima app funge!")
