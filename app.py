6d2c07a038c648b0ca7575909f01372524e97c05
glrbr <gloria12brivio@gmail.com>
2025-10-18 17:40:22 +0200

    Maiuscola

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as sl
 #Titolo della pagina
 sl.title("Geografia interattiva")
 #Sottotitolo della pagina
-sl.write("ecco il mio progetto di esame")
+sl.write("Ecco il mio progetto di esame")
 #Collegamento con immagine
 sl.image("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Map_of_the_world_with_countries_and_dependencies.svg/1024px-Map_of_the_world_with_countries_and_dependencies.svg.png")
 #Messaggio di successo
