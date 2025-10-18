a2afd77c067215de7a195d6ccd0380c4f252ad35
glrbr <gloria12brivio@gmail.com>
2025-10-18 18:10:20 +0200

    inseriti 3 animali dell africa

--- app.py
+++ app.py
@@ -8,11 +8,27 @@ sl.image("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Map_of_the_w
 #sezione animali
 sl.subheader("Animali del mondo")
 #creiamo un bottone
-if sl.button("Animali dell'Africa"):
-    #Questo codice viene eseguito solo quando clicco il bottone
-    sl.image("https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/African_Bush_Elephant.jpg/500px-African_Bush_Elephant.jpg")
-    sl.write("Elefante africano")
-    sl.write("vive nelle savane")
+# --- Sezione animali dell'Africa ---
+sl.subheader("🦁 Animali dell'Africa")
 
-#Messaggio di successo
-sl.success("La mia prima app funziona!")
+# Bottone 1: Elefante
+if sl.button("🐘 Elefante"):
+    sl.image("https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/African_Bush_Elephant.jpg/500px-African_Bush_Elephant.jpg")
+    sl.write("*Elefante Africano*")
+    sl.write("Vive nelle savane e foreste. È il più grande animale terrestre!")
+    sl.info("📍 Continente: Africa")
+
+# Bottone 2: Leone
+if sl.button("🦁 Leone"):
+    sl.image("https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Lion_waiting_in_Namibia.jpg/500px-Lion_waiting_in_Namibia.jpg")
+    sl.write("*Leone*")
+    sl.write("Il re della savana! Vive in gruppi chiamati branchi.")
+    sl.info("📍 Continente: Africa")
+
+# Bottone 3: Giraffa
+if sl.button("🦒 Giraffa"):
+    sl.image("https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Giraffe_standing.jpg/400px-Giraffe_standing.jpg")
+    sl.write("*Giraffa*")
+    sl.write("L'animale più alto del mondo! Il suo collo può essere lungo 2 metri.")
+    sl.info("📍 Continente: Africa")
+
