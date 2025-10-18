3ab8368d29089bbad379993394502988874e279b
glrbr <gloria12brivio@gmail.com>
2025-10-18 18:03:04 +0200

    aggiunto animale

--- app.py
+++ app.py
@@ -5,5 +5,14 @@ sl.title("Geografia interattiva")
 sl.write("Ecco il mio progetto di esame")
 #Collegamento con immagine
 sl.image("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Map_of_the_world_with_countries_and_dependencies.svg/1024px-Map_of_the_world_with_countries_and_dependencies.svg.png")
+#sezione animali
+sl.subheader("Animali del mondo")
+#creiamo un bottone
+if sl.button("Animali dell'Africa")
+    #Questo codice viene eseguito solo quando clicco il bottone
+    sl.image("https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/African_Bush_Elephant.jpg/500px-African_Bush_Elephant.jpg")
+    sl.write("Elefante africano")
+    sl.write("vive nelle savane")
+
 #Messaggio di successo
-sl.success("la mia prima app funge!")
+sl.success("La mia prima app funziona!")
