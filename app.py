421d481598acdcbc30c6918500b1710c37a47e40
glrbr <gloria12brivio@gmail.com>
2025-10-18 17:38:50 +0200

    inseriti commenti

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as sl
+#Titolo della pagina
 sl.title("Geografia interattiva")
+#Sottotitolo della pagina
 sl.write("ecco il mio progetto di esame")
+#Collegamento con immagine
 sl.image("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Map_of_the_world_with_countries_and_dependencies.svg/1024px-Map_of_the_world_with_countries_and_dependencies.svg.png")
+#Messaggio di successo
 sl.success("la mia prima app funge!")
