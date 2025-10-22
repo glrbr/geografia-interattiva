9097c59ba1cfc976b758a6a8e44012ce82ea04b7
glrbr <gloria12brivio@gmail.com>
2025-10-22 20:51:38 +0200

    climi e biomi tropicali ed equatoriali

--- app.py
+++ app.py
@@ -34,4 +34,41 @@ if sl.button("Zona Polare"):
     sl.info("Bioma")
     sl.image("https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Mt_Herschel%2C_Antarctica%2C_Jan_2006.jpg/1024px-Mt_Herschel%2C_Antarctica%2C_Jan_2006.jpg")
     sl.write("Aree deserte")
-    sl.write("fauna:pinguni,foche e albatro reale")
+    sl.write("fauna: pinguni,foche e albatro reale")
+
+# --- Sezione zona tropicale---
+sl.subheader("zona tropicale")
+
+# Bottone 1:fascia subpolare
+if sl.button("zona tropicale umida"):
+    sl.info("Clima")
+    sl.write(" 2 stagioni ")
+    sl.write("stagine delle piogge e stagine secca")
+    sl.info("Bioma")
+    sl.title("giungla")
+    sl.image("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Fall_on_the_Yukon_Flats_NWR.jpg/500px-Fall_on_the_Yukon_Flats_NWR.jpg")
+    sl.write("Flora: foreste tropicale con piante alofire")
+    sl.write("Fauna:grandi mammiferi predatori come tigri o giaguari, molte speci di uccelli e rettili")
+    sl.title("savana")
+    sl.image("https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Icebreaker_in_greenland_2.JPG/500px-Icebreaker_in_greenland_2.JPG")
+    sl.write("flora:erbe e arbusti spinosi")
+    sl.write("fauna:grandi mammiferi (come leoni, elefanti, zebre...), struzzi, uccelli serpentari e serpenti")
+if sl.button("zona tropicale secca"):
+    sl.info("Clima")
+    sl.write("scarse piogge con le temperature più alte della terra")
+    sl.info("Bioma")
+    sl.write("Flora: piante xerofite")
+    sl.write("Fauna:uccelli rapaci, rettili, roditori, insetti, e altri mammiferi")
+
+# --- Sezione zona equatorial---
+sl.subheader("zona equatoriale")
+
+# Bottone 1:fascia subpolare
+
+sl.info("Clima")
+sl.write("da 27° a 29°in tutto l'anno e molte piogge(3500mm l'anno)")
+sl.info("Bioma")
+sl.title("foresta equatoriale")
+sl.image("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Fall_on_the_Yukon_Flats_NWR.jpg/500px-Fall_on_the_Yukon_Flats_NWR.jpg")
+sl.write("Flora: molte spece di piante di altezze elevate per avere la luce del sole ")
+sl.write("Fauna:più do metà delle spece terrestri")
